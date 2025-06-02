8c20b1ce9685ab05f2426c8d643ffc7f95d606a9
waytrue <waytrue@waytruedeMini.lan>
2025-06-02 13:53:09 +0800

    kickstart.nvim

---
+++ home/config/nvim/lua/custom/plugins/org.lua
@@ -0,0 +1,35 @@
+return {
+  "chipsenkbeil/org-roam.nvim",
+  tag = "0.1.1",
+  dependencies = {
+    {
+      "nvim-orgmode/orgmode",
+      tag = "0.3.7",
+      event = "VeryLazy",
+      ft = { "org" },
+      config = function()
+        -- Setup orgmode
+        require("orgmode").setup {
+          org_agenda_files = "~/org_roam/**/*",
+          org_default_notes_file = "~/org_roam/refile.org",
+        }
+
+        -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+        -- add ~org~ to ignore_install
+        -- require('nvim-treesitter.configs').setup({
+        --   ensure_installed = 'all',
+        --   ignore_install = { 'org' },
+        -- })
+      end,
+    },
+  },
+  config = function()
+    require("org-roam").setup {
+      directory = "~/org_roam",
+      -- optional
+      --org_files = {
+      --  "~/",
+      --},
+    }
+  end,
+}
